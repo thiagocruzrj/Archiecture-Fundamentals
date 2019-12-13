9e367c2ce2b30da66a3fee6aed4b540e36332b0e
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2019-12-12 23:06:21 -0300

    Anti-pattern Service Locator sample

--- Controllers/Service2LocatorController.cs
+++ Controllers/Service2LocatorController.cs
@@ -1,7 +1,16 @@
+using System;
+using ArchiectureFundamentals.Cases;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace ArchiectureFundamentals.Controllers
 {
-    public class Service2LocatorController
+    public class Service2LocatorController : Controller
     {
-        
+        public void Index([FromServices] IServiceProvider serviceProvider)
+        {
+            // If not registered, return null
+            serviceProvider.GetRequiredService<IUserServices>().AddUser(new User());
+        }
     }
 }
--- Controllers/ServiceLocatorController.cs
+++ Controllers/ServiceLocatorController.cs
@@ -1,7 +1,22 @@
+using System;
+using ArchiectureFundamentals.Cases;
+using Microsoft.AspNetCore.Mvc;
+
 namespace ArchiectureFundamentals.Controllers
 {
-    public class ServiceLocatorController
+    public class ServiceLocatorController : Controller
     {
-        
+        private readonly IServiceProvider _serviceProvider;
+
+        public ServiceLocatorController(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public void Index()
+        {
+            // If not registered, it return null
+            //_serviceProvider.GetRequiredService<IUserServices>.AddUser(new User());
+        }
     }
 }
